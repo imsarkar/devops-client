e2858b3c912c9a773b84664b3571f33b4357c6a1
imsarkar <hraj0105@gmail.com>
2022-06-27 15:04:10 +0530

    initialization of preview branch

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js!</a> <a>Dev deploy preview must not pushed on deployment</a>
         </h1>
 
         <p className={styles.description}>
